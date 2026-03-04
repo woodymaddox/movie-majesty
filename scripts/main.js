339aa29d0e07781b3c2299dc856dd90e841c6ba7
WOODY MADDOX <WOODY.E.MADDOX@GMAIL.COM>
2026-03-03 20:19:45 -0600

    Added main css module and initial styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,9 @@
 
+
+
 import { generateMoviesHTML } from './movie-converter.js';
 import { renderMoviesToDOM } from './movie-render.js';
 
+
+const movieHTML = generateMoviesHTML();
 renderMoviesToDOM(movieHTML);
